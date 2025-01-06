c4907320292a42cc4d20e9f122d34b0835c54ef6
iannbraga <bragaprestes@gmail.com>
2025-01-05 23:20:44 -0300

    Xadrez - Movimentação de peças

--- xadrez/js/script.js
+++ xadrez/js/script.js
@@ -1,6 +1,7 @@
 function chessGame() {
   return {
     board: [],
+    selectedCell: null, // Armazena a célula selecionada (linha e coluna)
 
     init() {
       // Inicializa o tabuleiro com peças padrão
@@ -14,6 +15,23 @@ function chessGame() {
         ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
         ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
       ];
+    },
+
+    selectCell(row, col) {
+      // Se já há uma célula selecionada, tenta mover a peça
+      if (this.selectedCell) {
+        const [selectedRow, selectedCol] = this.selectedCell;
+
+        // Mover a peça para a nova posição
+        this.board[row][col] = this.board[selectedRow][selectedCol];
+        this.board[selectedRow][selectedCol] = '';
+
+        // Limpa a seleção
+        this.selectedCell = null;
+      } else {
+        // Seleciona a célula atual
+        this.selectedCell = [row, col];
+      }
     }
   };
 }
